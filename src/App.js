4f34de416b1b8e3ad5f2439adb3d70dee438ac9f
Aditi Kumari <aditib929@gmail.com>
2020-05-14 19:51:49 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-// import Firstpg from './component/firstpg';
-// import Modals from './component/modal';
-// import Second from "./component/second";
-// import Third from "./component/third";
 import React from "react";
 import {BrowserRouter, Switch, Route, Link, NavLink} from "react-router-dom";
 import Header from "./myproject/views/header/header";
 import Footer from "./myproject/views/header/footer";
 import Body from "./myproject/views/header/body";
 import Form1 from './myproject/views/header/form';
-import Login from "./myproject/views/header/login";
 import SignUp from "./myproject/views/header/form2";
 
 class App extends React.Component {
